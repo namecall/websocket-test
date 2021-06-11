4852fa695077f8d2c0e224ec6ebe6f965cca0725
namecall <85725697+namecall@users.noreply.github.com>
2021-06-11 02:38:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const websocket = new socket.Server({port:4532})
 // Connection Event
 websocket.on("connection", function(client) {
   console.log("Created new connection!")
-  client.on("message", function(message) {
-     websocket.clients.forEach(function each(click) {
-         if (click !== websocket) {
-	  client.send(message);
-         }
-      })
-   })
+    client.on("message", function(message) {
+        websocket.clients.forEach(function each(click) {
+            if (click !== websocket) {
+	            client.send(message);
+            }
+        })
+    })
 })
