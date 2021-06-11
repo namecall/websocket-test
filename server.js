d051c9cc59f185304f61cdf1efeddeac3281ac51
namecall <85725697+namecall@users.noreply.github.com>
2021-06-11 01:01:13 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+// Environment
+const socket = require("ws")
+
+// Connection
+let web_cache = []
+const websocket = new socket.Server({port:4532})
+
+// Connection Event
+websocket.on("connection", function(client) {
+  console.log("Created new connection!")
+  client.on("message", function(message) {
+     websocket.clients.forEach(function each(click) {
+         if (click !== websocket) {
+					 client.send(message);
+         }
+      })
+   })
+})
